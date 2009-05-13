4fce0ab146d3d9a5a23d259c3d7a94eaadbf9e5a
Miško Hevery <misko@hevery.com>
2009-05-13 18:56:21 +0000

    Refactored the Compilation Unit Again * Two pass parser. First to get class structure, second to analyze * We can now parse classes and inner classes * We can now parse the field declarations

--- core/src/main/java/com/google/test/metric/javasrc/JavaSrcRepository.java
+++ core/src/main/java/com/google/test/metric/javasrc/JavaSrcRepository.java
@@ -15,6 +15,10 @@
  */
 package com.google.test.metric.javasrc;
 
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 import antlr.RecognitionException;
 import antlr.TokenStreamException;
 
@@ -25,26 +29,61 @@ import com.google.test.metric.ClassRepository;
 public class JavaSrcRepository implements ClassRepository {
 
 	private final ClassPath classPath;
+	private Map<String, ClassInfo> classes = new HashMap<String, ClassInfo>();
+  private final ClassRepository parentRepository;
 
-	public JavaSrcRepository(ClassPath classPath) {
-		this.classPath = classPath;
+	public JavaSrcRepository(ClassRepository parentRepository, ClassPath classPath) {
+		this.parentRepository = parentRepository;
+    this.classPath = classPath;
 	}
 
 	public ClassInfo getClass(String clazzName) {
-		String src = clazzName.replace('.', '/') + ".java";
-		JavaLexer lexer = new JavaLexer(classPath.getResourceAsStream(src));
+		ClassInfo info = getCachedClass(clazzName);
+		if (info != null) {
+			return info;
+		}
+		parse(clazzName);
+		info = classes.get(clazzName);
+		if (info != null) {
+			return info;
+		}
+		return parentRepository.getClass(clazzName);
+	}
+
+	private void parse(String clazzName) {
+		String src = clazzName.replace('.', '/').replaceAll("\\$.*", "") + ".java";
+		InputStream srcStream = classPath.getResourceAsStream(src);
+		if (srcStream == null) {
+			return;
+		}
+		JavaLexer lexer = new JavaLexer(srcStream);
 		JavaRecognizer recognizer = new JavaRecognizer(lexer);
 		recognizer.getASTFactory().setASTNodeClass(CommonASTWithLine.class);
 		JavaTreeParser treeParser = new JavaTreeParser();
+		Qualifier qualifier = new Qualifier();
+    treeParser.builder = new CompilationUnitBuilder(this, qualifier);
 		try {
 			recognizer.compilationUnit();
+			qualifier.compilationUnit(recognizer.getAST());
 			treeParser.compilationUnit(recognizer.getAST());
-			return treeParser.builder.build();
 		} catch (RecognitionException e) {
 			throw new RuntimeException(e);
 		} catch (TokenStreamException e) {
 			throw new RuntimeException(e);
 		}
 	}
+
+	@Override
+	public String toString() {
+		return classes.toString();
+	}
+
+  public void addClass(ClassInfo info) {
+    classes.put(info.getName(), info);
+  }
+
+  public ClassInfo getCachedClass(String clazzName) {
+    return classes.get(clazzName);
+  }
 
 }
