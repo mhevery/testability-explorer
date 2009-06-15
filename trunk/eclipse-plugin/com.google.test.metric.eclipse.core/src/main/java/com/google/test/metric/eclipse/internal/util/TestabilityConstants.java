579534a248753a2b1ba8ace3ba3d7f4d4a4d6c8e
shyamseshadri <shyamseshadri@472494f5-f44b-0410-b89f-bda6e703c5e4>
2009-06-15 16:59:07 +0000

    Change plugin.xml to use annotations connected to markers to display icons. Added TE icon. Reviewed in person by kslundberg

--- trunk/eclipse-plugin/com.google.test.metric.eclipse.core/src/main/java/com/google/test/metric/eclipse/internal/util/TestabilityConstants.java
+++ trunk/eclipse-plugin/com.google.test.metric.eclipse.core/src/main/java/com/google/test/metric/eclipse/internal/util/TestabilityConstants.java
@@ -39,7 +39,7 @@ public class TestabilityConstants {
   public static final String CONFIGURATION_ATTR_WHITELIST =
     "testability.launch.configuration.whitelist";
   
-  public static final String TESTABILITY_MARKER_TYPE = "com.google.test.metric.testabilityMarker";
+  public static final String TESTABILITY_MARKER_TYPE = "com.google.test.metric.eclipse.ui.testabilityMarker";
   
   public static final int MAX_ACCEPTABLE_COST = 100;
   public static final int MAX_EXCELLENT_COST = 20;
