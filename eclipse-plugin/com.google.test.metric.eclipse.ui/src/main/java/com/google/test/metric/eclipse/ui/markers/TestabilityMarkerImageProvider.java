eb12f31e1438ba55c160c1408dc15d71a3cfb743
Shyam Seshadri <shyamseshadri@gmail.com>
2009-06-16 00:00:21 +0000

    Add TE.jpg which is used as markers for issues instead of generic projects.gif previously

--- eclipse-plugin/com.google.test.metric.eclipse.ui/src/main/java/com/google/test/metric/eclipse/ui/markers/TestabilityMarkerImageProvider.java
+++ eclipse-plugin/com.google.test.metric.eclipse.ui/src/main/java/com/google/test/metric/eclipse/ui/markers/TestabilityMarkerImageProvider.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.google.test.metric.eclipse.ui.markers;
 
 import org.eclipse.core.resources.IMarker;
@@ -6,12 +21,10 @@ import org.eclipse.ui.internal.ide.IMarkerImageProvider;
 public class TestabilityMarkerImageProvider implements IMarkerImageProvider {
 
   public TestabilityMarkerImageProvider() {
-    // TODO Auto-generated constructor stub
   }
 
   public String getImagePath(IMarker marker) {
-    // TODO Auto-generated method stub
-    return "icons/projects.gif";
+    return "icons/TE.jpg";
   }
 
 }
